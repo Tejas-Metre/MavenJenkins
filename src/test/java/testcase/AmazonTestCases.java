269c75a74a6e00414a5c14f8c91d2d8a47cdebd4
tejas.metre <tejas.metre@skillnetinc.com>
2026-06-04 17:00:17 +0530

    AmazonTestCases.java:second commit class is modify

--- src/test/java/testcase/AmazonTestCases.java
+++ src/test/java/testcase/AmazonTestCases.java
@@ -5,16 +5,16 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
+//import org.testng.annotations.Listeners;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-
-import utilities.excel.ListenerClass;
 
 import Basic.BaseClass;
 import PageAction.AmazonHomePageAction;
 import PageAction.AmazonSearchedPage;
 import dataproviders.DataProviderClass;
+import utilities.excel.ListenerClass;
 
 @Listeners(value={ListenerClass.class})
 public class AmazonTestCases extends BaseClass {
@@ -24,35 +24,39 @@ public class AmazonTestCases extends BaseClass {
 	AmazonHomePageAction ap;
 	
 	@BeforeClass
-	void beforeClass()
+	public void beforeClass()
 	{
 		setUp();
 		
 	}
 	
 	@BeforeMethod
-	void beforeMethod()
+	public void beforeMethod()
 	{
 		//reload();
 		reload();
 	}
 	
 	@AfterClass
-	void afterClass()
+	public void afterClass()
 	{
 		//close();
 	}
 	
-	@Parameters("title1")
+	@Parameters("titles1")
 	@Test
-	void verifyTitle(String expectedTitle)
+	public void verifyTitle(String expectedTitle) throws InterruptedException
 	{
 		 ap=new AmazonHomePageAction();
+		 Thread.sleep(3000);
+		 System.out.println("Expected: " + expectedTitle);
+		    System.out.println("Actual: " + driver.getTitle());
+
 		 Assert.assertTrue(ap.checkTitle(expectedTitle));
 	}
 	
 	@Test()
-	void verifyURL()
+	public void verifyURL()
 	{
 		//gp= new GooglePage();
 		//System.out.println("Url status : "+gp.checkURL(prop.getProperty("url")));
@@ -91,8 +95,8 @@ public class AmazonTestCases extends BaseClass {
 	
 	
 	//@Parameters("watchTitle1")
-	@Test(priority = 2,dependsOnMethods = {"verifyTitle"},dataProvider = "getSearchData", dataProviderClass = DataProviderClass.class)
-	void verifyDataSearch(String data)
+	@Test(priority = 2,/*dependsOnMethods = {"verifyTitle"},*/dataProvider = "getSearchData", dataProviderClass = DataProviderClass.class)
+	public	void verifyDataSearch(String data)
 	{
 		//String SearchData="STAD Solution";
 		ap= new AmazonHomePageAction();
